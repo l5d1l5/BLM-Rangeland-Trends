34b1f875458096ece84034756644d48627573f0b
andy <andy.kleinhesselink@gmail.com>
2021-03-17 12:17:11 -0700

    Add grazing information from Chris Hansen

--- code/compile_allotment_database.R
+++ code/compile_allotment_database.R
@@ -1,7 +1,13 @@
 library(sf)
 library(tidyverse)
 library(ggridges)
-allotment_sf <- readRDS(file = 'output/BLM_cleaned_shape_sf.RDS')
+library(lubridate)
+allotment_id <- read_csv('output/allotment_table.csv') %>% 
+  select( uname, ADMIN_ST, ADM_OFC_CD, ALLOT_NO, ALLOT_NAME ) %>%
+  mutate( `Bill Allot Number` = paste0( ADMIN_ST, ALLOT_NO))
+
+allotment_sf <- readRDS(file = 'output/BLM_cleaned_shape_sf.RDS') %>%
+  left_join(allotment_id , by = c('uname', 'ADMIN_ST', 'ADM_OFC_CD', 'ALLOT_NAME'))
 
 elevation <- read_csv('data/MT_allotment_data/MT_allotment_elevation_by_feature.csv') %>% 
   select( mean, uname ) %>% 
@@ -21,6 +27,18 @@ burns <- read_csv('data/MT_allotment_data/MT_allotment_burns_by_feature.csv') %>
   pivot_longer(cols = -uname, names_to = 'year', values_to = 'burned') %>%
   mutate( year = as.numeric( str_extract( year, '\\d{4}$'))) 
 
+grazing <- read_rds('data/aum_all.rds') %>% 
+  left_join( 
+    allotment_id %>% 
+    select( `Bill Allot Number`, uname, ADMIN_ST, ADM_OFC_CD, ALLOT_NAME, ALLOT_NO), 
+  by = "Bill Allot Number") %>% 
+  mutate( year = year( mdy(`Bill Begin Date`)))
+
+grazing %>%
+  select( uname, year , starts_with('Bill'), ADM_OFC_CD, ALLOT_NAME, ALLOT_NO) %>%
+  arrange( uname, year ) %>% 
+  write_rds('data/allotment_grazing_data.rds')
+
 # Clean up allotment administrative information 
 
 allotment_sf %>% 
@@ -38,9 +56,8 @@ climate %>%
   left_join(cover ) %>%
   left_join( burns ) %>% 
   select(uname, year, pdsi, pr, tavg, afgAGB:pfgAGB, AFGC:TREE, burned) %>% 
-  pivot_longer(cols = c(pdsi:TREE, burned), names_to = 'type', values_to = 'response')  %>%
+  pivot_longer(cols = c(pdsi:TREE, burned), names_to = 'type', values_to = 'value')  %>%
+  filter( complete.cases(.)) %>%
   write_rds( file = 'data/allotment_data_long.rds')
-
-
 
 # 
